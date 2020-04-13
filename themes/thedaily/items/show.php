4d54d4c8e9456c604cb96fdf99a058424b96a7ec
cmccullough012 <cxm1880@rit.edu>
2020-04-13 15:58:09 -0400

    Move item show nav to top

--- themes/thedaily/items/show.php
+++ themes/thedaily/items/show.php
@@ -23,6 +23,12 @@ echo head(array('title' => metadata('item', array('Dublin Core', 'Title')), 'bod
 ?>
 
 <h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
+<nav>
+<ul class="item-pagination navigation">
+    <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
+    <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
+</ul>
+</nav>
 
 <?php if ($hasImages): ?>
     <ul id="itemfiles" <?php echo (count($images) == 1) ? 'class="solo"' : ''; ?>>
@@ -91,11 +97,6 @@ echo head(array('title' => metadata('item', array('Dublin Core', 'Title')), 'bod
 
 <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
 
-<nav>
-<ul class="item-pagination navigation">
-    <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
-    <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
-</ul>
-</nav>
+
 
 <?php echo foot(); ?>
